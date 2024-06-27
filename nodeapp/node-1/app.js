329c56c1954580dca431b17c6d290916836eaede
parvez76 <parvez3676@gmail.com>
2024-06-27 17:43:56 +0600

    initial commit

---
+++ nodeapp/node-1/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/app', (req, res) => {
+    return res.status(200).send({"message": "app res from server 1"});
+});
+
+const port = 5001;
+
+app.listen(port, () => console.log(`server is up and running from ${port}`));
